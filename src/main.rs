6184ccaf18e3af5c2d26f0d82d3b10566dd68a90
Reilly Wood <reilly.wood@icloud.com>
2022-05-20 20:49:26 -0700

    things are working!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,19 @@
 use std::{
-    ffi::c_void,
-    mem::MaybeUninit,
+    ffi::{c_void, OsString},
+    mem::MaybeUninit, os::windows::prelude::OsStringExt, slice,
 };
 
 use anyhow::Result;
 use windows::{
-    core::GUID,
+    core::{GUID, PWSTR, PCWSTR, Interface},
     Win32::{
         Foundation::HANDLE,
-        System::Com,
+        System::Com::{self, CoTaskMemFree},
         System::Com::{CoInitializeEx},
         UI::Shell::{
             FOLDERID_RecycleBinFolder, IShellItem,
-            SHGetKnownFolderItem, KF_FLAG_DEFAULT, SIGDN_NORMALDISPLAY, IEnumShellItems, BHID_EnumItems,
-        },
+            SHGetKnownFolderItem, KF_FLAG_DEFAULT, SIGDN_NORMALDISPLAY, IEnumShellItems, BHID_EnumItems, IShellItem2, PropertiesSystem::{PROPERTYKEY, PropVariantChangeType, PROPVAR_CHANGE_FLAGS, PVCHF_DEFAULT, PropVariantToBSTR}, SHCOLUMNINFO,
+        }, Globalization::lstrlenW,
     },
 };
 
@@ -23,7 +23,7 @@ fn main() -> Result<()> {
     unsafe {
         CoInitializeEx(std::ptr::null(), Com::COINIT_APARTMENTTHREADED)?;
 
-        // Couldn't find this in the `windows` crate
+        // Couldn't find these in the `windows` crate
         // {43826D1E-E718-42EE-BC55-A1E261C37BFE}
         let IID_IShellItem = GUID::from_values(
             0x43826d1e,
@@ -32,8 +32,12 @@ fn main() -> Result<()> {
             [0xbc, 0x55, 0xa1, 0xe2, 0x61, 0xc3, 0x7b, 0xfe],
         );
 
+        // {9B174B33-40FF-11d2-A27E-00C04FC30871}
+        let PSGUID_DISPLACED = GUID::from_values(0x9b174b33, 0x40ff, 0x11d2, [0xa2, 0x7e, 0x0, 0xc0, 0x4f, 0xc3, 0x8, 0x71]);
+        let PID_DISPLACED_FROM: u32 = 2;
+
         // TODO: make sure we free this
-        let mut recycle_bin = MaybeUninit::<*mut IShellItem>::uninit();
+        let mut recycle_bin = MaybeUninit::<Option<IShellItem2>>::uninit();
 
         SHGetKnownFolderItem(
             &FOLDERID_RecycleBinFolder,
@@ -43,16 +47,90 @@ fn main() -> Result<()> {
             recycle_bin.as_mut_ptr() as *mut *mut c_void,
         )?;
 
-        let recycle_bin = recycle_bin.assume_init();
+        let recycle_bin = recycle_bin.assume_init().expect("not initialized");
         
-        // let name = recycle_bin.as_ref().unwrap().GetDisplayName(SIGDN_NORMALDISPLAY)?;
+        let name = recycle_bin.GetDisplayName(SIGDN_NORMALDISPLAY)?;
+        let path_copy = convert_to_os_string(name);
+        dbg!(path_copy);
 
-        // this crashes with a STATUS_ACCESS_VIOLATION
-        let pesi: IEnumShellItems = (*recycle_bin).BindToHandler(None, &BHID_EnumItems)?;
+        let pesi: IEnumShellItems = recycle_bin.BindToHandler(None, &BHID_EnumItems)?;
+        let mut fetched: u32 = 0;
 
-        dbg!(pesi);
+        loop {
+            // Not sure if this is the best way to get data out of Next
+            let mut arr= [None];
+            pesi.Next(&mut arr, &mut fetched)?;
+
+            if fetched == 0 {
+                break;
+            }
+
+            match &arr[0] {
+                Some(i) => {
+                    println!("{:?}", get_display_name(i)?);
+                    let i2: IShellItem2 = i.cast()?;
+                    // i2.GetPropertyDescriptionList()
+
+                    // {9B174B33-40FF-11d2-A27E-00C04FC30871}
+// #define PSGUID_DISPLACED    {0x9b174b33, 0x40ff, 0x11d2, 0xa2, 0x7e, 0x0, 0xc0, 0x4f, 0xc3, 0x8, 0x71}
+// DEFINE_GUID(FMTID_Displaced, 0x9b174b33, 0x40ff, 0x11d2, 0xa2, 0x7e, 0x0, 0xc0, 0x4f, 0xc3, 0x8, 0x71);
+// #define PID_DISPLACED_FROM  2
+                    let l = PROPERTYKEY{ fmtid: PSGUID_DISPLACED, pid: PID_DISPLACED_FROM };
+                    // let l: SHCOLUMNINFO;
+                    let asdf = i2.GetProperty(&l)?;
+
+
+                    let b = PropVariantToBSTR(&asdf)?;
+                    // PropVariantChangeType()
+                    dbg!(b);
+
+                    // asdf.try_into()
+
+                    // let a = &asdf.Anonymous.Anonymous.Anonymous.bstrVal;
+
+                    // &asdf.
+                    // WHY IS THIS CRASHING?
+                    // let sadfasdgf = OsString::from_wide(a.as_wide());
+                    // dbg!();
+                },
+                None => {break;},
+            }
+        }
     }
 
     println!("Done!");
     Ok(())
+}
+
+fn get_display_name(psi: &IShellItem) -> Result<OsString> {
+    unsafe {
+        let name = psi.GetDisplayName(SIGDN_NORMALDISPLAY)?;
+        let path_copy = convert_to_os_string(name);
+        // dbg!(path_copy);
+
+        let ret = path_copy.clone();
+
+        CoTaskMemFree(name.0 as *const c_void);
+        Ok(ret)
+    }
+}
+
+// void PrintDisplayName(IShellItem *psi, SIGDN sigdn, PCTSTR pszLabel)
+// {
+//  LPWSTR pszName;
+//  HRESULT hr = psi->GetDisplayName(sigdn, &pszName);
+//  if (SUCCEEDED(hr)) {
+//   _tprintf(TEXT("%s = %ws\n"), pszLabel, pszName);
+//   CoTaskMemFree(pszName);
+//  }
+// }
+
+// no idea why this is so hard
+fn convert_to_os_string(name: PWSTR) -> OsString {
+    let converted = PCWSTR(name.0);
+    // why is this conversion needed?
+    unsafe {
+        let path_ref: &[u16] = slice::from_raw_parts(name.0, lstrlenW(converted) as usize);
+        OsString::from_wide(path_ref)
+    }
 }
